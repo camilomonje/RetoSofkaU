59077fcb3ef780d65c8ba02af1d9d206fa25bd97
Luis Camilo Monje Perdomo <lumonje12@gmail.com>
2021-09-24 14:34:10 -0500

    Iniciando Proyecto

---
+++ src/main/java/com/sofka/RetoSofkaU/RetoSofkaUApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.RetoSofkaU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoSofkaUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoSofkaUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/RetoSofkaU/RetoSofkaUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.RetoSofkaU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoSofkaUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
